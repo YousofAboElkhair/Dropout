c25846e3a754f71e9de00e3d1b321f0ebea72335
YousofAboElkhair <88256260+YousofAboElkhair@users.noreply.github.com>
2021-10-14 14:53:53 +0200

    Create Dropout.py

---
+++ Dropout.py
@@ -0,0 +1,22 @@
+model = keras.Sequential([
+    layers.Dense(128, activation='relu', input_shape=input_shape),
+    layers.Dropout(0.3),
+    layers.Dense(64, activation='relu'),
+    layers.Dropout(0.3),
+    layers.Dense(1)
+])
+
+model.compile(
+    optimizer='adam',
+    loss='mae',
+)
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=512,
+    epochs=50,
+    verbose=0,
+)
+history_df = pd.DataFrame(history.history)
+history_df.loc[:, ['loss', 'val_loss']].plot()
+print("Minimum Validation Loss: {:0.4f}".format(history_df['val_loss'].min()))
